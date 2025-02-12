e8b3960daf5c8dfe1c2df5fd42df2812cb29f258
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2025-02-12 12:21:43 -0800

    Reorganizing File Structure

--- sdk/src/abis/DeedNFTABI.ts
+++
@@ -1,6 +0,0 @@
-export const DeedNFTABI = [
-  // Copy ABI from contract compilation
-  // This is crucial for contract interaction
-] as const;
-
-// Create similar files for other contracts 
--- sdk/src/contracts/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all contract implementations
-export * from './DeedNFTContract';
-export * from './SubdivideContract';
-export * from './FractionalizeContract';
-export * from './ValidatorRegistryContract';
-export * from './BaseContract'; 
