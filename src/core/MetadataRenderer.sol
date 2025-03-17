b52b8dd7a503a5757267f7e826edde2b8385bf92
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2025-03-16 21:31:45 -0700

    Update MetadataRenderer

--- src/core/MetadataRenderer.sol
+++ src/core/MetadataRenderer.sol
@@ -6,8 +6,12 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/StringsUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/utils/Base64Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+// Libraries
 import "../libraries/StringUtils.sol";
 import "../libraries/JSONUtils.sol";
+
+// Interfaces
 import "./interfaces/IERC7572.sol";
 import "./interfaces/IDeedNFT.sol";
 
