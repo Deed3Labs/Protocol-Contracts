866379fcb240396ad4425d69f6314be94a82aef0
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2025-07-28 02:51:35 -0700

    Minor fix to AppKitProvider and Header

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -272,7 +272,9 @@ const Header = ({ children }: HeaderProps) => {
 
           {/* Desktop Actions */}
           <div className="hidden md:flex items-center space-x-2">
-            <appkit-button />
+            <div className="border border-black/10 dark:border-transparent rounded-full">
+              <appkit-button />
+            </div>
             <Link to="/profile">
               <Button variant="outline" size="sm" className="h-9 px-3 border-black/10 dark:border-white/10">
                 <User className="w-4 h-4 mr-1" />
@@ -292,7 +294,9 @@ const Header = ({ children }: HeaderProps) => {
 
           {/* Mobile Actions */}
           <div className="md:hidden flex items-center space-x-2">
-            <appkit-button />
+            <div className="border border-black/10 dark:border-transparent rounded-full">
+              <appkit-button />
+            </div>
             <Link to="/profile">
               <Button variant="outline" size="icon" className="border-black/10 dark:border-white/10">
                 <User className="w-4 h-4" />
