f204990c75378a3a272e01ee1e0a226a1331cf37
Isaiah "KyngKai" Litt <kyngkai909@gmail.com>
2026-03-10 20:19:03 -0700

    minor improvements

--- app/src/components/portfolio/ProfileMenu.tsx
+++ app/src/components/portfolio/ProfileMenu.tsx
@@ -68,6 +68,12 @@ const ProfileMenu = ({ isOpen, onClose, user, onOpenXMTP }: ProfileMenuProps) =>
     return conversationMessages[conversationMessages.length - 1];
   };
 
+  const formatAddress = (address: string) => {
+    if (!address) return '';
+    if (address.length <= 12) return address;
+    return `${address.slice(0, 6)}...${address.slice(-4)}`;
+  };
+
   return (
     <AnimatePresence>
       {isOpen && (
